acba123f14511018b348d18aa8587a6f5bc98337
Jenny Winters <jfwintersdev@gmail.com>
2025-03-17 18:22:45 +0000

    basic dark mode

--- src/app/shared/navbar.component.ts
+++ src/app/shared/navbar.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-navbar',
-  template: ` <p>navbar goes here</p> `,
+  imports: [MatButtonModule],
+  template: `
+    <p>navbar goes here</p>
+    <button mat-flat-button>Click me!</button>
+  `,
 })
 export class NavbarComponent {}
